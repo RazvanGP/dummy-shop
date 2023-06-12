e88332da9b8ca20b8c683ab7ab2fbf66de009e2a
Razvan Plingu <plingu.razvan@gmail.com>
2023-06-12 19:38:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/home";
+import About from "./pages/about";
+import Navigation from "./components/nav/nav";
+import ProductDetails from "./pages/productDetails";
+import FormPage from "./pages/formPage";
+import LiveFormPage from "./pages/liveFormPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation />
+      <main>
+        <Routes>
+          <Route index element={<Home />} />{'landing page'}
+          <Route path="/about" element={<About />} />
+          <Route path="/details/:id" element={<ProductDetails />} />
+          <Route path="/form" element={<FormPage />}/>
+        </Routes>
+      </main>
+      <footer></footer>
+    </Router>
   );
 }
 
